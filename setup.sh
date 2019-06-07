ce871fce6fb0cf9984df38aee104b4f97c3c9ba3
slickFix <sidd.200894@gmail.com>
2019-06-07 17:20:28 +0530

    downloads VGG19.mat and coco2014 training dataset

---
+++ setup.sh
@@ -0,0 +1,5 @@
+mkdir data
+cd data
+wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+unzip train2014.zip
