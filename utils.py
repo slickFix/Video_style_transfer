1f2387c6f80fcab4f8875dc0df4f19fb79a0add7
slickFix <sidd.200894@gmail.com>
2019-06-07 15:26:47 +0530

    implemented get_files, get_img and list_files in utils.py

--- utils.py
+++ utils.py
@@ -6,3 +6,27 @@ Created on Fri Jun  7 15:03:34 2019
 @author: siddharth
 """
 
+import scipy.misc, numpy as np,os
+
+def get_img(src,img_size = False):
+    img = scipy.misc.imread(src,mode='RGB')
+    
+    if not (len(img.shape) ==3 and img.shape[2]==3):
+        img = np.dstack((img,img,img))
+        
+    if img_size != False:
+        img = scipy.misc.imresize(img,img_size)
+        
+    return img
+
+def list_files(in_path):
+    files = [] 
+    for (dir_path,dir_names,file_names) in os.walk(in_path):
+        files.extend(file_names)
+        break
+    
+    return files
+
+def get_files(img_dir):
+    files = list_files(img_dir)
+    return [os.path.join(img_dir,x) for x in files]
