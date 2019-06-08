ae224a2688540e17d36c5301bb9da80f6fa33b0e
slickFix <sidd.200894@gmail.com>
2019-06-08 14:56:08 +0530

    implemented validate_options() and main() functions in evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -46,9 +46,51 @@ def build_parser():
     
     return parser
 
+
+def validate_options(options):
+    
+    assert os.path.exists(options.checkpoint_dir),'checkpoint not found!'
+    assert os.path.exists(options.in_path),'Input path not found!'
+    
+    if os.path.isdir(options.out_path):
+        assert os.path.exists(options.out_path),'output dir not found'
+    
+    assert options.batch_size >0
+    
+
 def main():
     # creating input argument parser
     parser = build_parser()
+    
+    options = parser.parse_args()
+    
+    validate_options(options)
+    
+    # if input path is file
+    if not os.path.isdir(options.in_path):
+        # if output path exists and is directory
+        if os.path.exists(options.out_path) and os.path.isdir(options.out_path):
+            out_path = \
+                        os.path.join(options.out_path,os.path.basename(options.in_path))
+        
+        else:
+            out_path = options.out_path
+            
+        ffwd_single_img(options.in_path,out_path,options.checkpoint_dir,device = options.device)
+    
+    # if input path is directory
+    else:
+        files = list_files(options.in_path)
+        full_in = [os.path.join(options.in_path,x) for x in files]
+        full_out = [os.path.join(options.out_path,x) for x in files]
+        
+        if options.allow_different_dimensions:
+            ffwd_different_dimensions(full_in,full_out,options.checkpoint_dir,
+                                      device_t = options.device,batch_size = options.batch_size)
+            
+        else:
+            ffwd_img(full_in,full_out,options.checkpoint_dir,device_t=options.device,
+                 batch_size = options.batch_size)
 
 if __name__ == '__main__':
     main()    
