21ed1535f428fc859cc328c91c5746a0a04d7185
slickFix <sidd.200894@gmail.com>
2019-06-10 14:20:38 +0530

    implemented feed_forward_video() case in evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -14,12 +14,88 @@ from argparse import ArgumentParser
 from collections import defaultdict
 import datetime as dt
 
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import moviepy.video.io.ffmpeg_writer as ffmpeg_writer
+
 BATCH_SIZE = 4
 DEVICE = '/gpu:0'
 
 
-def ffwd_img(data_in,paths_out,checkpoint_dir,device_t='/gpu:0',batch_size = 4):
+def ffwd_video(path_in,path_out,checkpoint_dir,device_t = '/gpu:0',batch_size = 4):
+    ''' feed forward video '''
     
+    # defining video rendering variables
+    video_clip = VideoFileClip(path_in,audio=False)
+    video_writer = ffmpeg_writer.FFMPEG_VideoWriter(path_out,video_clip.size,video_clip.fps,codec = 'libx264',
+                                                    preset = 'medium',bitrate = '2000k',
+                                                    audiofile =path_in,threads=None,
+                                                    ffmpeg_params=None)
+    
+    # defining tensorflow variables 
+    g = tf.Graph()
+    soft_config = tf.ConfigProto(allow_soft_placement = True)
+    soft_config.gpu_options.allow_growth = True
+    
+    # starting the tensorflow session
+    with g.as_default(), g.device(device_t), tf.Session(config=soft_config) as sess:
+        
+        batch_shape = (batch_size,video_clip.size[1], video_clip.size[0], 3)
+        
+        # defining placeholder
+        vid_ph = tf.placeholder(tf.float32,shape=batch_shape,name='vid_ph')
+        
+        # forward propogation (building the graph)
+        preds = transform_net.net(vid_ph)
+        
+        # defining saver
+        saver = tf.train.Saver()
+        
+        # restoring the saved model
+        
+        if os.path.isdir(checkpoint_dir):
+            ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+            else:
+                raise Exception("No checkpoint found...")
+        else:
+            saver.restore(sess, checkpoint_dir)
+        
+        x = np.zeros(batch_shape,dtype = np.float32)
+        
+        # function to generate styled video (batch images) and writing
+        def style_and_write(count):
+            
+            # for batch size not complete case
+            for i in range(count,batch_size):
+                x[i] = x[count-1] # using last frame(received from .iter_frames) to fill remaing x (batch size not complete case)
+            
+            # running the graph to style video
+            _preds = sess.run(preds,feed_dict = {vid_ph:x})
+            
+            for i in range(0,count):
+                video_writer.write_frame(np.clip(_preds,0,255).astype(np.uint8))
+                
+        frame_count = 0 # the frame count written to x
+        
+        for frame  in video_clip.iter_frames():
+            
+            x[frame_count] = frame
+            frame_count +=1
+            
+            if frame_count == batch_size:
+                
+                style_and_write(frame_count)
+                frame_count = 0
+        
+        # for last batch where no of images is less than the batch_size                
+        if frame_count !=0:
+            style_and_write(frame_count)
+            
+        video_writer.close()
+
+def ffwd_img(data_in,paths_out,checkpoint_dir,device_t='/gpu:0',batch_size = 4):
+    ''' feed forward image function '''
     assert len(paths_out)>0
     
     # checking if the input path is string type
